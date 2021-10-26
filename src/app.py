99eadb7bb04443f7cedbc1b6dc7d4d7aa5ed4ce5
Levente <l.zolyomi@student.tue.nl>
2021-10-26 22:36:47 +0200

    updated env

--- src/app.py
+++ src/app.py
@@ -1,10 +1,7 @@
 #--------------Import libraries
 import pandas as pd
 import streamlit as st
-import numpy as np
-import os
-from os import listdir
-import platform
+import plotly.express as px
 
 #--------------Import custom functions, objects
 from Data import Dataset 
@@ -28,11 +25,18 @@ st.set_page_config(layout="wide")
 #---------------Layout of the app
 ### Filtering
 tops = st.sidebar.selectbox("Select challenge you are interested in:", topics)
-
 objs_filtered = [d for d in data_objs if tops in d.topics]
 data_display = st.sidebar.multiselect("Directly select datasets to display:", [d.name for d in objs_filtered])
 objs_display = return_objs(data_display, objs_filtered)
 
+###------------ Displaying data
 for obj in objs_display:
+    # Loop through each data object
+    f = obj.filename
     obj.display()
     obj.display_plots()
+    col1, col2, col3 = st.columns(3)
+    if f == 'jobs_Brabant':
+        y = col1.selectbox('Choose the sector you want to plot', obj.filename.columns[2:])
+        barfig = px.bar(obj.df, x='Municipality/Sector', y=y)
+        st.plotly_chart(barfig)
