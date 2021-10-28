ca3cb0998d6a348a0a4166a55fdfa2bf678edfdb
Levente <l.zolyomi@student.tue.nl>
2021-10-28 17:53:30 +0200

    final touches

--- src/app.py
+++ src/app.py
@@ -46,18 +46,18 @@ if data_display!=[]:
         data is about.')
 
 else:
-    st.image('src/banner.jpg')
+    st.image('src/banner.jpg', width=1100)
     st.markdown('**Welcome to the dashboard for the Healthy Brabantine City Deal Challenge!**')
     st.text('')
     st.markdown('This dashboard will guide you through the several datasets that are collected for you to use in your projects.\
-    To help you get started, a short explanation about how to interact with the dashboard will follow.')
+    To help you get started, here is a short explanation on how to interact with the dashboard.')
     st.text('') 
     st.markdown('On the left you can see\
     two menus, one to choose a challenge and another to choose datasets. You can select the challenge you want to look into by choosing\
     one of the three challenges in the top select menu. Now, in the lower select menu you can choose which datatsets belonging\
-    to the chosen challenge you want to see. Each dataset contains an explanation, an option to download it as a csv to your computer\
+    to the chosen challenge you want to see, and you can choose as many as you want to view at the same time. Each dataset contains an explanation, an option to download it as a csv to your computer\
     and the table showing what data is in it. Some of the datasets also have explanatory plots to help you understand better what the\
-    data is about.')
+    data is about. You can use the interactive tools to change the graphics.')
 
 ###------------ Displaying data
 for obj in objs_display:
@@ -72,6 +72,7 @@ for obj in objs_display:
         y = col1.selectbox('Choose the sector you want to plot', obj.df.columns[2:])
         st.markdown('The plot shows the amount of jobs in the chosen sector per municipality')
         barfig = px.bar(obj.df, x='Municipality/Sector', y=y)
+        barfig.update_traces(marker_color='#cb1337')
         st.plotly_chart(barfig, use_container_width=True)
 
     if f == 'youth_labour_participation':
@@ -85,6 +86,7 @@ for obj in objs_display:
         y2 = col1.selectbox('Select which health characteristic you want to look at', obj.df.columns[1:])
         st.markdown('The plot shows the percentage of people falling in the chosen characteristic per municipality')
         barf = px.bar(obj.df, x='Municipalities (2021)', y=y2)
+        barf.update_traces(marker_color='#cb1337')
         st.plotly_chart(barf, use_container_width=True)
     
     if f == 'renewable_electricity':
@@ -99,6 +101,7 @@ for obj in objs_display:
         df_filtered = obj.df[obj.df['Category'] == category]
         df_quarters = filter_quarters(df_filtered, 'Periods')
         barfig2 = px.bar(df_quarters, y='Change_baseline', x='Periods')
+        barfig2.update_traces(marker_color='#cb1337')
         st.plotly_chart(barfig2, use_container_width=True)
 
     if f == 'municipality_data':
@@ -106,6 +109,7 @@ for obj in objs_display:
         municip = col2.multiselect('Select a municipality', obj.df['Name of municipality'].unique(), default=["'s-Hertogenbosch"])
         filtered = obj.df[obj.df['Name of municipality'].isin(municip)]
         fig4 = px.bar(filtered, x='year', y=feature, color='Name of municipality')
+        fig4.update_traces(marker_color='#cb1337')
         st.plotly_chart(fig4,use_container_width=True)
 
     if f=='water_treatment':
@@ -113,6 +117,7 @@ for obj in objs_display:
         year = col2.select_slider('Choose the year', obj.df['Periods'].unique())
         filt = obj.df[(obj.df['Periods'] == year) & (obj.df['Regions'] != 'Nederland')] 
         barchart = px.bar(filt, y=feat, x='Regions')
+        barchart.update_traces(marker_color='#cb1337')
         st.plotly_chart(barchart, use_container_width=True)
 
     if f=='bankruptcy':
@@ -121,6 +126,7 @@ for obj in objs_display:
         region_filter = only_total[only_total['Regions'] == sel_region]
         quarterly = filter_quarters(region_filter, 'Periods')
         fig5 = px.bar(quarterly, x='Periods', y='PronouncedBankrupcies_1')
+        fig5.update_traces(marker_color='#cb1337')
         st.plotly_chart(fig5, use_container_width=True)
 
     if f=='student_debt':
@@ -128,13 +134,15 @@ for obj in objs_display:
         feature2 = col2.selectbox('Select a feature', obj.df.columns[3:])
         filtered2 = obj.df[obj.df['Characteristic'] == char]
         fig6 = px.bar(filtered2, x='Period', y=feature2)
+        fig6.update_traces(marker_color='#cb1337')
         st.plotly_chart(fig6, use_container_width=True)
 
     if f=='housing_stock':
         #NEED FIX for piechart
         feat2 = col1.selectbox('Choose a feature', obj.df.columns[3:])
-        pie3 = px.pie(obj.df, names=None, values=feat2)
-        st.plotly_chart(pie3)
+        pie3 = px.pie(obj.df, names='municipalities_Brabant', values=feat2,title='Distribution of selected feature in municipalities')
+        pie3.update_traces(textinfo='none')
+        st.plotly_chart(pie3, use_container_width=True)
 
     if f=='land_usage':
         municip = col1.selectbox('Choose municipality', obj.df['municipalities_Brabant'])
