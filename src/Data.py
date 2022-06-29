270b05d08cd9621abaa488619bf50704035beb7b
Levente <l.zolyomi@student.tue.nl>
2022-06-29 20:54:13 +0200

    download csv without index column

--- src/Data.py
+++ src/Data.py
@@ -1,42 +1,43 @@
 import pandas as pd
-import streamlit as st 
+import streamlit as st
 
 ####### Importing functions
 
+
 class Dataset:
     def __init__(self, filename, name, descr, health, water, living) -> None:
-        self.filename = filename #filename WITHOUT .parquet extension
-        self.name = name #Name to be displayed
-        self.descr = descr #SHORT description of the data
-        #Will hold True or False whether dataset can be used for given challenge
+        self.filename = filename  # filename WITHOUT .parquet extension
+        self.name = name  # Name to be displayed
+        self.descr = descr  # SHORT description of the data
+        # Will hold True or False whether dataset can be used for given challenge
         self.health = health
         self.water = water
         self.living = living
-    
+
     def open_file(self):
-        self.df = pd.read_parquet(f'src/data/{self.filename}.parquet')
+        self.df = pd.read_parquet(f"src/data/{self.filename}.parquet")
 
     def return_topics(self):
         """Stores the topics present in a list"""
         topics = []
         if self.health:
-            topics.append('Health')
+            topics.append("Health")
         if self.water:
-            topics.append('Water')
+            topics.append("Water")
         if self.living:
-            topics.append('Living environment')
+            topics.append("Living environment")
         self.topics = topics
 
-    def download(self): 
+    def download(self):
         """
         Returns a streamlit download button with the csv file attached
         NOTE only run after open_file, otherwise the dataframe is not there"""
-        csv = self.df.to_csv().encode('utf-8')
+        csv = self.df.to_csv(index=False).encode("utf-8")
         return st.download_button(
-            label='Download csv',
-            data = csv,
-            file_name=f'{self.filename}.csv',
-            mime='text/csv'
+            label="Download csv",
+            data=csv,
+            file_name=f"{self.filename}.csv",
+            mime="text/csv",
         )
 
     def display(self):
@@ -44,14 +45,11 @@ class Dataset:
         Displays the name, description
         the dataframe and a download button for csv"""
         ret_elements = []
-        ret_elements.append(st.markdown(f'## {self.name} \n #### {self.descr}'))
-        self.open_file() #opens file as dataframe
-        ret_elements.append(self.download()) 
-        ret_elements.append(
-            st.dataframe(self.df.head(100))
-        )
+        ret_elements.append(st.markdown(f"## {self.name} \n #### {self.descr}"))
+        self.open_file()  # opens file as dataframe
+        ret_elements.append(self.download())
+        ret_elements.append(st.dataframe(self.df.head(100)))
         return ret_elements
-
 
     def __str__(self):
         return self.name
